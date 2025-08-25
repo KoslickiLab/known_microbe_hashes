ddf87dee44909a75726987fa47dc8ff9cc3d81b5
David Koslicki <dmkoslicki@gmail.com>
2025-08-24 21:43:36 -0400

    remove vestigial code from the GenBank WGS inspiration

--- AllTheBacteria/scripts/analysis/import_to_duckdb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/time -v python ingest_hashes_to_duckdb.py --input /scratch/dmk333_new/GenBank_WGS_analysis/analysis/reduced_parquet --db wgs_database.db --mode per-ksize --threads 64 --create-index --analyze
