a39a28e83ebee8c28bb5265b0277bc9d120c0ea2
David Koslicki <dmkoslicki@gmail.com>
2025-09-05 21:46:37 -0400

    add rest of scripts for TSA #10

---
+++ GenBank_TSA/scripts/dump_dedup_and_import.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eou pipefail
+# export
+/usr/bin/time -v python /scratch/dmk333_new/known_microbe_hashes/shared_scripts/hash_export.py --input-root /scratch/genbank_tsa/tsa_sketches --out-root /scratch/genbank_tsa/tsa_sketches_parquet --mod 512 --workers 96 --compression zstd --progress-log-every 1000 > export_tsa.log 2>&1
+
+# dedup
+/usr/bin/time -v python /scratch/dmk333_new/known_microbe_hashes/shared_scripts/hash_dedup.py --non-dedup-root /scratch/genbank_tsa/tsa_sketches_parquet --out-root /scratch/genbank_tsa/tsa_sketches_dedup --threads 96 --by-ksize > dedup_tsa.log 2>&1
+
+# import
+/usr/bin/time -v /scratch/dmk333_new/known_microbe_hashes/shared_scripts/hash_ingest_duckdb.py --dedup-root /scratch/genbank_tsa/tsa_sketches_dedup --db genbank_tsa_unique_hashes.db --threads 96 --mode create --analyze > make_duck_db_from_dedup.log 2>&1
