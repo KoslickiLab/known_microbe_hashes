d8af6f039c2f0df1fad723a61e0758f32f8656be
David Koslicki <dmkoslicki@gmail.com>
2025-09-12 15:28:47 -0400

    file rename

--- Obelisks/scripts/download_gtdb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget -P ../data https://data.ace.uq.edu.au/public/gtdb/data/releases/release226/226.0/genomic_files_reps/gtdb_genomes_reps_r226.tar.gz
-cd ../data
-tar -xzvf gtdb_genomes_reps_r226.tar.gz
