5a9533590b2a1f524d8958fa1cf16c7a35568a93
David Koslicki <dmkoslicki@gmail.com>
2025-10-01 10:12:44 -0400

    get viral genomes for #14

---
+++ Serratus_viruses/scripts/get_data.sh
@@ -0,0 +1,9 @@
+cd ../data/sequences
+wget https://serratus-public.s3.amazonaws.com/rdrp_contigs/rdrp_contigs.tar.gz
+tar -xzvf rdrp_contigs.tar.gz
+wget https://serratus-public.s3.amazonaws.com/assemblies/master_table_assemblies.tar.gz
+tar -xzvf master_table_assemblies.tar.gz
+wget https://lovelywater2.s3.amazonaws.com/assembly/micro/rdrp1.mu.fa
+wget https://github.com/rcedgar/palmdb/raw/refs/heads/main/2021-03-14/serratus.fa.gz
+gunzip serratus.fa.gz
+aws s3 cp s3://lovelywater2 --recursive --no-sign-request --exclude "*" --include "*.fasta"
